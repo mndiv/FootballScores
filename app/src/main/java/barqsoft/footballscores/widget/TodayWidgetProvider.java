9b0f67ee6764c7470c2735f30b13198f7bd097f3
mndivya <nagadivya.m@gmail.com>
2015-10-22 01:40:01 -0400

    widget with Static data

---
+++ app/src/main/java/barqsoft/footballscores/widget/TodayWidgetProvider.java
@@ -0,0 +1,52 @@
+package barqsoft.footballscores.widget;
+
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.widget.RemoteViews;
+
+import barqsoft.footballscores.MainActivity;
+import barqsoft.footballscores.R;
+
+/**
+ * Created by KeerthanaS on 10/22/2015.
+ */
+public class TodayWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        int teamResourceId = R.drawable.arsenal;
+        String description = "Arsenal";
+        // Perform this loop procedure for each Today widget
+        for (int appWidgetId : appWidgetIds) {
+            int layoutId = R.layout.widget_today_small;
+            RemoteViews views = new RemoteViews(context.getPackageName(), layoutId);
+
+
+            // Add the data to the RemoteViews
+            views.setImageViewResource(R.id.widget_icon, teamResourceId);
+            // Content Descriptions for RemoteViews were only added in ICS MR1
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+                setRemoteContentDescription(views, description);
+            }
+
+            views.setTextViewText(R.id.widget_today_match, "2");
+
+            Intent launchIntent =  new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+    private void setRemoteContentDescription(RemoteViews views, String description) {
+        views.setContentDescription(R.id.widget_icon, description);
+    }
+}
